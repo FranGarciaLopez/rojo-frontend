a61f904ff591cb61ffdaf81ec2ce9f371f21975e
Fran <frangalo34@gmail.com>
2025-01-04 14:35:24 +0100

    order api calls

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const apiClient = axios.create({
@@ -6,35 +7,39 @@ const apiClient = axios.create({
           headers: { 'Content-Type': 'application/json' },
 });
 
-export const fetchCities = () => apiClient.get('/cities/cities');
-export const fetchUserData = (authToken) =>
-          apiClient.get('/user', { headers: { Authorization: `Bearer ${authToken}` } });
-
-export const getUsers = () => apiClient.get('/users');
+// Authentication
 export const registerUser = (userData) => apiClient.post('/register', userData);
 export const loginUser = (userData) => apiClient.post('/login', userData);
-export const fetchActivitiesCategories = () => apiClient.get('/categories/categories');
-export const updateUserPreferences = (authToken, preferences) =>
-          apiClient.put('/update-preferences', preferences, {
-                    headers: { Authorization: `Bearer ${authToken}` },
-          });
-
+export const fetchUserData = (authToken) =>
+          apiClient.get('/user', { headers: { Authorization: `Bearer ${authToken}` } });
 export const resetPassword = async ({ email }) => {
           const response = await apiClient.put('/forgotpassword', { email });
           return response;
 };
 
-export const getEvents = (authToken) => {
-          const response = apiClient.get('/events/events', {
+// Users
+export const getUsers = () => apiClient.get('/users');
+export const updateUserPreferences = (authToken, preferences) =>
+          apiClient.put('/update-preferences', preferences, {
                     headers: { Authorization: `Bearer ${authToken}` },
           });
-          return response;
 
-};
+// Cities and Categories
+export const fetchCities = () => apiClient.get('/cities/cities');
+export const fetchActivitiesCategories = () => apiClient.get('/categories/categories');
 
-export const getEventById = (authToken, id) => {
-          const response = apiClient.get(`/events/events/${id}`, {
+// Events
+export const getEvents = (authToken) =>
+          apiClient.get('/events/events', {
                     headers: { Authorization: `Bearer ${authToken}` },
           });
-          return response;
-}
+
+export const getEventById = (authToken, id) =>
+          apiClient.get(`/events/events/${id}`, {
+                    headers: { Authorization: `Bearer ${authToken}` },
+          });
+
+export const updateEvent = (authToken, event, data) =>
+          apiClient.patch(`/events/editevent/${event}`, data, {
+                    headers: { Authorization: `Bearer ${authToken}` },
+          });
