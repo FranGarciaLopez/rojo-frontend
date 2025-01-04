0eaa955e2530d7a3e67764770eb3a12ac89b3848
Fran <frangalo34@gmail.com>
2025-01-04 14:36:17 +0100

    update create event form styles

--- src/components/molecules/CreateEventForm.jsx
+++ src/components/molecules/CreateEventForm.jsx
@@ -145,131 +145,76 @@ export const CreateEvent = () => {
     return (
         <>
             <NavBar />
-            <div className="centered-elements">
-                <div className="flex flex-col h-full p-6 gap-6 xl:flex-row">
-                    <form onSubmit={handleSubmit}>
-                        <div className="w-full items-center gap-2">
-                            <i
-                                className="fas fa-arrow-left cursor-pointer"
-                                onClick={() => window.history.back()}
-                            ></i>
+            <div className="h-16"></div> {/* Spacer to prevent overlap */}
+            <div className="mx-auto px-5 h-screen justify-center items-center flex flex-col gap-6">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+                    <div className="w-full items-center gap-2">
+                        <i
+                            className="fas fa-arrow-left cursor-pointer"
+                            onClick={() => window.history.back()}
+                        ></i>
 
-                            <span className="font-heading-5 font-bold text-default-font">
-                                {" "}
-                                Create New Event
-                            </span>
-                        </div>
-
-                        <div className="flex w-full flex-col items-center justify-center gap-2 rounded-md px-6 py-6">
-                            <div className="flex w-full items-center gap-2 border-b border-solid border-neutral-border px-6 py-6">
-                                <span className="text-heading-3 font-bold text-default-font">
-                                    Add details
-                                </span>
-                                <span className="text-body font-body font-bold text-subtext-color">
-                                    tags, source, comments
-                                </span>
-                            </div>
-                            <div className="flex w-full flex-col items-start gap-6 px-6 py-6">
-                                <span className="text-body-bold font-body-bold text-default-font">
-                                    Title
-                                </span>
-                                <InputText
-                                    type="text"
-                                    placeholder="Title"
-                                    value={title}
-                                    onChange={(e) => setTitle(e.target.value)}
-                                    className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 min-w-[400px]"
-                                    required
-                                />
-                                <div className="flex w-full flex-col items-start gap-2">
-                                    <div className="flex w-full items-center gap-2">
-                                        <select
-                                            value={city}
-                                            onChange={(e) => setCity(e.target.value)}
-                                            ref={refs.city}
-                                            className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full"
-                                            required
-                                        >
-                                            <option value=""> Choose your City </option>
-                                            {cities.map((city) => (
-                                                <option key={city._id} value={city.name}>
-                                                    {city.name}
-                                                </option>
-                                            ))}
-                                        </select>
-                                    </div>
-
-                                    <select
-
-                                        value={category}
-                                        onChange={(e) => setCategory(e.target.value)}
-                                        ref={refs.category}
-                                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full"
-                                        required
-                                    >
-                                        <option value="">     Choose a Category    </option>
-                                        {categories.map((category) => (
-                                            <option key={category._id} value={category._id}>
-                                                {category.categoryName}
-                                            </option>
-                                        ))}
-
-                                    </select>
-                                </div>
-
-                                <div className="flex w-full flex-col items-start gap-2">
-                                    <span className="text-body-bold font-body-bold text-default-font">
-                                        Comment
-                                    </span>
-                                    <textarea
-                                        rows="5"
-                                        cols="50"
-                                        type="text"
-                                        placeholder="Description"
-                                        value={description}
-                                        onChange={(e) => setDescription(e.target.value)}
-                                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 min-w-full"
-                                        required
-                                    />
-                                </div>
-                                <input
-                                    type="datetime-local"
-                                    value={dateTime}
-                                    onChange={(e) => setDateTime(e.target.value)}
-                                    className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 min-w-[400px]"
-                                    required
-                                />
-                                <form className="flex h-full w-full flex-col items-center gap-6 bg-default-background px-6 py-10 mobile:h-full mobile:w-full">
-                                    <SubframeCore.Icon
-                                        className="text-heading-1 font-heading-1 text-brand-700"
-                                        name="FeatherImage"
-                                    />
-                                    <UploadImage setPhotos={setPhotos} />
-                                </form>
-
-                                <Buttons
-                                    type="submit"
-                                    value="Create Event"
-                                    className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
-                                />
-
-                                {isUploading && (
-                                    <div>
-                                        <div className="progress-bar" style={{ width: `${uploadProgress}%` }} />
-                                        <div className="progress-text">{uploadProgress}%</div>
-                                    </div>
-                                )}
-                                {uploadSuccess && (
-                                    <div className="text-green-500">
-                                        Photos uploaded successfully!
-
-                                    </div>
-                                )}
-                                {error && <div className="text-red-500">{error}</div>}
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        <span className="font-heading-5 font-bold text-default-font">
+                            {" "}
+                            Create New Event
+                        </span>
+                    </div>
+                    <InputText
+                        type="text"
+                        placeholder="Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full"
+                        required
+                    />
+                    <select
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full"
+                        required
+                    >
+                        <option value="">Choose your City</option>
+                        {cities.map((city) => (
+                            <option key={city._id} value={city.name}>
+                                {city.name}
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full"
+                        required
+                    >
+                        <option value="">Choose a Category</option>
+                        {categories.map((category) => (
+                            <option key={category._id} value={category._id}>
+                                {category.categoryName}
+                            </option>
+                        ))}
+                    </select>
+                    <textarea
+                        rows="5"
+                        placeholder="Description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full"
+                        required
+                    />
+                    <input
+                        type="datetime-local"
+                        value={dateTime}
+                        onChange={(e) => setDateTime(e.target.value)}
+                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full"
+                        required
+                    />
+                    <UploadImage setPhotos={setPhotos} />
+                    <Buttons
+                        type="submit"
+                        value="Create Event"
+                        className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+                    />
+                </form>
             </div>
         </>
     );
