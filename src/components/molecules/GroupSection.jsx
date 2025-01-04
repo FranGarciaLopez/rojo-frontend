bab77e40e7fdf81faede51f767132b19788c5354
Fran <frangalo34@gmail.com>
2025-01-04 12:09:03 +0100

    add different layouts for mobile/web site (group button)

--- src/components/molecules/GroupSection.jsx
+++ src/components/molecules/GroupSection.jsx
@@ -1,6 +1,8 @@
 
 const GroupSection = ({ groupDetails, openEventGroupPage }) => (
-          <section className="flex flex-row items-center justify-between w-full px-6 py-4 bg-gray-50">
+          <section className="
+                    grid grid-cols-1 px-0
+                    md:flex flex-row items-center justify-between w-full px-6 py-4 bg-gray-50">
                     <h1>Explore Activities</h1>
                     <small>
                               {groupDetails.map((group) => (
@@ -8,7 +10,7 @@ const GroupSection = ({ groupDetails, openEventGroupPage }) => (
                                                   {group.interestedEvents?.length > 0 ? (
                                                             <button
                                                                       onClick={() => openEventGroupPage(group.interestedEvents[0], group._id)}
-                                                                      className="mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                                                                      className="w-full mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
                                                             >
                                                                       Join Group Chat
                                                             </button>
